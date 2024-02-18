eabd346da5184ed1e3c8d477146e8384d4844fe6
Raihan Mahmud <raihan_uits@outlook.com>
2024-02-18 19:40:13 +0600

    adding data

--- client/src/medicine.js
+++ client/src/medicine.js
@@ -1,3 +1,24 @@
 export const data = [
-    
+    {
+        'genericName': 'Domperidon Maleate',
+        'brandName': 'Ridon',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Tablet',
+        'strength': '10 mg',
+        'packSize': '10 X 10',
+        'unitPrice': 2.50,
+        'therapeuticClass': 'Gastrointestinal'
+    },
+    {
+        'genericName': 'Paracetamol',
+        'brandName': 'Tamen',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Tablet',
+        'strength': '500 mg',
+        'packSize': '20 X 10',
+        'unitPrice': 1.20,
+        'therapeuticClass': 'Gastrointestinal'
+    }
 ]
