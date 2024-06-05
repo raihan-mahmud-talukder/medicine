e7dd9cb7897d7355c7095579173b14afbd887613
Raihan Mahmud <raihan_uits@outlook.com>
2024-06-05 17:12:07 +0600

    'confgured'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,6 @@ export const App = () => {
             <th>Generic Name</th>
             <th>Brand Name</th>
             <th>Company</th>
-            <th>Drug Type</th>
             <th>Dosage Form</th>
             <th>Strength</th>
             <th>Pack Size</th>
@@ -20,19 +19,59 @@ export const App = () => {
           </tr>
           {data.map((item, index) => {
             return (
-              <tr>
-                <td>{(index + 1) < 9 ? `0${index + 1}` : (index + 1)}</td>
-                <td>{item.genericName}</td>
+              <tr key={index}>
+                <td>{(index + 1) < 10 ? `0${index + 1}` : (index + 1)}</td>
+                <td>
+                  {typeof (item.genericName) === 'object' ?
+                    Object.keys(item.genericName).map((name, index) => {
+                      return (
+                        <ul key={index}><li>{item.genericName[name]}</li></ul>
+                      )
+                    })
+                    : item.genericName}
+                </td>
                 <td>{item.brandName}</td>
                 <td>{item.companyName}</td>
-                <td>{item.drugType}</td>
-                <td>{item.dosageForm}</td>
-                <td>{item.strength}</td>
-                <td>{item.packSize}</td>
-                <td>BDT {item.unitPrice}</td>
+                <td>
+                  {typeof (item.dosageForm) === 'object' ?
+                    Object.keys(item.dosageForm).map((name, index) => {
+                      return (
+                        <ul key={index}><li>{item.dosageForm[name]}</li></ul>
+                      )
+                    })
+                    : item.dosageForm}
+                </td>
+                <td>
+                  {typeof (item.strength) === 'object' ?
+                    Object.keys(item.strength).map((name, index) => {
+                      return (
+                        <ul key={index}><li>{item.strength[name]}</li></ul>
+                      )
+                    })
+                    : item.strength}
+                </td>
+                <td>
+                  {typeof (item.packSize) === 'object' ?
+                    Object.keys(item.packSize).map((name, index) => {
+                      return (
+                        <ul key={index}><li>{item.packSize[name]}</li></ul>
+                      )
+                    })
+                    : item.packSize}
+                </td>
+                <td>
+                  {typeof (item.unitPrice) === 'object' ?
+                    Object.keys(item.unitPrice).map((name, index) => {
+                      return (
+                        <ul key={index}><li>BDT {item.unitPrice[name]}</li></ul>
+                      )
+                    })
+                    : `BDT ${item.dosageForm !== 'Suspension' ? Number(item.unitPrice).toFixed(2) : item.unitPrice}`}
+                </td>
                 <td>{item.therapeuticClass}</td>
               </tr>
-            )})}
+            )
+          })}
         </tbody>
       </table>
     </>
--- client/src/medicine.js
+++ client/src/medicine.js
@@ -1,6 +1,6 @@
 export const data = [
     {
-        'genericName': 'Domperidon Maleate',
+        'genericName': 'Domperidon',
         'brandName': 'Ridon',
         'companyName': 'Eskayef',
         'drugType': 'Allopathic',
@@ -9,16 +9,136 @@ export const data = [
         'packSize': '10 X 10',
         'unitPrice': 2.50,
         'therapeuticClass': 'Gastrointestinal'
-    },
-    {
+    }, {
+        'genericName': 'Esomeprazole',
+        'brandName': 'Esoral',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': ['Tablet', 'Capsule', 'Powder', 'Mups'],
+        'strength': '20 mg',
+        'packSize': ['08 X 14', '10 X 10', '20 X 01', '05 X 12'],
+        'unitPrice': ['6.00', '7.00', '7.00', '10.00'],
+        'therapeuticClass': 'Gastrointestinal'
+    }, {
+        'genericName': 'Fexofenadine',
+        'brandName': 'Dinafex',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Tablet',
+        'strength': '120 mg',
+        'packSize': '04 X 10',
+        'unitPrice': 9.00,
+        'therapeuticClass': 'Antihistamine'
+    }, {
+        'genericName': 'Montelukast',
+        'brandName': 'Lumona',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Tablet',
+        'strength': '10 mg',
+        'packSize': '03 X 14',
+        'unitPrice': 12.00,
+        'therapeuticClass': 'Respiratory'
+    }, {
+        'genericName': ['Naproxen', 'Esomeprazole'],
+        'brandName': 'Naprox Plus',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Tablet',
+        'strength': ['500 mg', '20 mg'],
+        'packSize': '04 X 10',
+        'unitPrice': 15.00,
+        'therapeuticClass': 'Pain Killer'
+    }, {
+        'genericName': 'Omeprazole',
+        'brandName': 'Losectil',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': ['Capsule', 'Powder'],
+        'strength': '20 mg',
+        'packSize': ['12 X 10', '50 X 01'],
+        'unitPrice': ['5.00', '6.00'],
+        'therapeuticClass': 'Gastrointestinal'
+    }, {
+        'genericName': 'Pantoprazole',
+        'brandName': 'Panoral',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Tablet',
+        'strength': '20 mg',
+        'packSize': '04 X 10',
+        'unitPrice': 5.00,
+        'therapeuticClass': 'Gastrointestinal'
+    }, {
         'genericName': 'Paracetamol',
         'brandName': 'Tamen',
         'companyName': 'Eskayef',
         'drugType': 'Allopathic',
         'dosageForm': 'Tablet',
         'strength': '500 mg',
+        'packSize': '10 X 20',
+        'unitPrice': 1.20,
+        'therapeuticClass': 'Analgesics & Antipyretic'
+    },
+    {
+        'genericName': 'Paracetamol',
+        'brandName': 'Tamen XR',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Tablet',
+        'strength': '665 mg',
+        'packSize': '10 X 10',
+        'unitPrice': 2.00,
+        'therapeuticClass': 'Musculoskeletal'
+    }, {
+        'genericName': ['Paracetamol', 'Caffeine'],
+        'brandName': 'Tamen Turbo',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Tablet',
+        'strength': ['500 mg', '65 mg'],
         'packSize': '20 X 10',
         'unitPrice': 1.20,
+        'therapeuticClass': 'Analgesic & Antipyretic'
+    }, {
+        'genericName': 'Rabeprazole',
+        'brandName': 'Rabifast',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Tablet',
+        'strength': '20 mg',
+        'packSize': '05 X 14',
+        'unitPrice': 7.00,
         'therapeuticClass': 'Gastrointestinal'
+    }, {
+        'genericName': ['Sodium Alginate', 'Sodium Bicarbonate', 'Calcium Carbonate'],
+        'brandName': 'Gastrum',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Suspension',
+        'strength': ['500 mg', '213 mg', '325 mg'],
+        'packSize': '200 ml',
+        'unitPrice': 250,
+        'therapeuticClass': 'Gastrointestinal'
+    }, {
+        'genericName': 'Sulcrafate',
+        'brandName': 'Protecto',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Suspension',
+        'strength': '1000mg/5ml',
+        'packSize': '200 ml',
+        'unitPrice': 400,
+        'therapeuticClass': 'Gastrointestinal'
+    }, {
+        'genericName': ['Vitamins B-Complex', 'Zinc'],
+        'brandName': 'Xinc B',
+        'companyName': 'Eskayef',
+        'drugType': 'Allopathic',
+        'dosageForm': 'Tablet',
+        'strength': 'N/A',
+        'packSize': '01 X 30',
+        'unitPrice': 3.50,
+        'therapeuticClass': 'Vitamins & Minerals'
     }
 ]
