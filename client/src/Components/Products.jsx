03440f126e7f3b367dd81c1d5cac03ea6d90df17
Raihan Mahmud <raihan_uits@outlook.com>
2024-06-20 15:30:36 +0600

    adding product

--- client/src/Components/Products.jsx
+++ client/src/Components/Products.jsx
@@ -1,20 +1,44 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
+import { data } from "../medicine"
 
 export const Products = () => {
     const [products, setProducts] = useState([])
     document.title = 'PRODUCTS'
 
+    function dataSort(data) {
+        let sortedData = data
+        
+        sortedData.forEach(element => {
+            
+            // element.genericName.sort((a, b) => {
+            //     let fa = a.toLowerCase().trim()
+            //     let fb = b.toLowerCase().trim()
+                
+            //     if (fa < fb) {
+            //         return -1
+            //     } else {
+            //         return 1
+            //     }
+            // })
+        })
+        return sortedData
+    }
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const data = (await axios.get('/api/products/getallproducts')).data
-                const sortedData = data.sort((a, b) => { return a.unitPrice - b.unitPrice })
-                setProducts(data)
+                setProducts(dataSort(data))
+
             } catch (error) { console.log(error) }
         }
         fetchData()
     }, [])
+
+
+    // sortedData.sort((a, b) => { return a.unitPrice - b.unitPrice })
+
 
     return (
         <>
@@ -26,15 +50,15 @@ export const Products = () => {
                         <tr>
                             <th>sl</th>
                             <th>Generic Name</th>
+                            <th>Strength</th>
                             <th>Dosage Form</th>
                             <th>Brand Name</th>
-                            <th>Strength</th>
                             <th>Company</th>
                             <th>Pack Size</th>
                             <th>Unit Price</th>
                             <th>Therapeutic Class</th>
                         </tr>
-                        {products.sort((a, b) => { return a.genericName - b.genericName }).map((product, index) => { return <Product product={product} key={product._id} index={index} /> })}
+                        {products.map((product, index) => { return <Product product={product} key={product._id} index={index} /> })}
                     </tbody>
                 </table>
             </div>
@@ -54,9 +78,9 @@ const Product = ({ product, index }) => {
         <tr>
             <td>{(index + 1) < 10 ? `0${index + 1}` : (index + 1)}</td>
             <td>{replaced(product.genericName)}</td>
-            <td>{product.dosageForm}</td>
-            <td>{product.brandName}</td>
             <td>{replaced(product.strength)}</td>
+            <td>{replaced(product.dosageForm)}</td>
+            <td>{product.brandName}</td>
             <td>{product.companyName}</td>
             <td>{product.packSize}</td>
             <td>BDT {product.unitPrice.toFixed(2)}</td>
